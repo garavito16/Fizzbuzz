a98556c5d6dca09b3ddce37df13c4b731f0fa0c8
garavito16 <lex_94@outlook.com>
2022-01-07 16:44:09 -0500

    mejora

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-for (let index = 1; index <= 100; index++) {
-    mensajeMultiplo3 = "Fizz";
-    mensajeMultiplo5 = "Buzz";
+let mensajeMultiplo3 = "Fizz";
+let mensajeMultiplo5 = "Buzz";
+let multiplo3 = false;
+let multiplo5 = false;
 
+for (let index = 1; index <= 100; index++) {
     multiplo5 = index%5 === 0;
     multiplo3 = index%3 === 0;
 
