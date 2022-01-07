5addeb7609c29717ff2368969d59f2eafc1c6904
garavito16 <lex_94@outlook.com>
2022-01-07 16:24:01 -0500

    subiendo cambios

---
+++ index.js
@@ -0,0 +1,17 @@
+for (let index = 1; index <= 100; index++) {
+    mensajeMultiplo3 = "Fizz";
+    mensajeMultiplo5 = "Buzz";
+
+    multiplo5 = index%5 === 0;
+    multiplo3 = index%3 === 0;
+
+    if (multiplo3 && multiplo5) {
+        console.log(mensajeMultiplo3 + mensajeMultiplo5)
+    } else if (multiplo3) {
+        console.log(mensajeMultiplo3);
+    } else if (multiplo5) {
+        console.log(mensajeMultiplo5);
+    } else {
+        console.log(index);
+    }
+}
